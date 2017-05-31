6072fb777a80406dde462268e225f6b5cb87a001
GIBMIT <GIBMIT@10.23.1.147>
2017-05-31 15:34:04 +0200

    Create coint class.

---
+++ src/ch/gibm/gfjw/dao/Coint.java
@@ -0,0 +1,36 @@
+package ch.gibm.gfjw.dao;
+
+public class Coint {
+
+	private String image;
+	private double value;
+	private String name;
+	
+	public Coint(String image, double value, String name) {
+		super();
+		this.image = image;
+		this.value = value;
+		this.name = name;
+	}
+	
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public double getValue() {
+		return value;
+	}
+	public void setValue(double value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
