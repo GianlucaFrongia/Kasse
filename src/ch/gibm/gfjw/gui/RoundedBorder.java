a90715495a8f83ca6d987db6038a0077fe838ec4
GIBMIT <GIBMIT@10.23.1.147>
2017-06-07 13:43:49 +0200

    Klasse für Runde Buttons

---
+++ src/ch/gibm/gfjw/gui/RoundedBorder.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2016 Coop, Informatik / Internet Entwicklung.
+ * Use is subject to license terms.
+ */
+
+package ch.gibm.gfjw.gui;
+
+import java.awt.Insets;
+import javax.swing.border.*;
+import static ch.gibm.gfjw.gui.Style.BLUE;
+
+public class RoundedBorder implements Border {
+
+    private int radius;
+
+
+    RoundedBorder(int radius) {
+        this.radius = radius;
+    }
+
+    public boolean isBorderOpaque() {
+        return true;
+    }
+
+	@Override
+	public Insets getBorderInsets(java.awt.Component c) {
+		return new Insets(this.radius/4, this.radius/4, this.radius/4, this.radius/4);
+	}
+
+
+	@Override
+	public void paintBorder(java.awt.Component c, java.awt.Graphics g, int x, int y, int width, int height) {
+		g.drawRoundRect(x, y, width-1, height-1, radius, radius);
+		g.setColor(BLUE);
+	}
+}
