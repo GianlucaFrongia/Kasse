bf5c8d8c22bf80379444e10ebbe449e335afe339
GIBMIT <GIBMIT@172.20.10.2>
2017-06-10 12:31:57 +0200

    Products lesen und schrieben

--- src/ch/gibm/gfjw/data/ProductData.java
+++ src/ch/gibm/gfjw/data/ProductData.java
@@ -5,12 +5,17 @@ import ch.gibm.gfjw.dto.Product;
 import ch.gibm.gfjw.dto.ProductImpl;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 /**
  * Created by gianluca on 06.06.17
@@ -20,21 +25,49 @@ public class ProductData implements ProductDao{
 
 
     public List<Product> getProductList() {
+    		
+    		ArrayList<Product> list = new ArrayList<>();
+    		JSONParser parser = new JSONParser();
 
+        try {
 
-        try (Reader reader = new FileReader("products.json")) {
-            Type type = new TypeToken<List<Product>>(){}.getType();
+            Object obj = parser.parse(new FileReader("products.json"));
 
-            List<Product> productList = new Gson().fromJson(reader, type);
+            JSONObject jsonObject = (JSONObject) obj;
+            System.out.println(jsonObject);
+            
+            list.add(new ProductImpl((String) jsonObject.get("name"),
+            		(Integer) jsonObject.get("id"), 
+            		(Double) jsonObject.get("resale"), 
+            		(Double) jsonObject.get("price")));
 
-            return productList;
-
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
-        }
+        } catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+        return list;
 
-        return null;
     }
+    	
+    	 	
+//        try (Reader reader = new FileReader("products.json")) {
+//            Type type = new TypeToken<List<ProductImpl>>(){}.getType();
+//
+//            List<Product> productList = new Gson().fromJson(reader, type);
+//
+//            return productList;
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return null;
+//    }
 
 
 
@@ -43,21 +76,38 @@ public class ProductData implements ProductDao{
     }
 
     public void addProduct(Product product) {
-        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    	
+    	JsonArray list = new JsonArray(obj);
+    	
+    	JSONObject obj = new JSONObject();
+        obj.put("id", product.getId());
+        obj.put("name", product.getName());
+        obj.put("resale", product.getResale());
+        obj.put("price", product.getPrice());
 
-        List<Product> productList = new ArrayList<Product>();
-        productList.add(product);
-
-        try (Writer writer = new FileWriter("products.json", true)) {
-            writer.write(gson.toJson(productList));
-            writer.close();
-
+        try (FileWriter file = new FileWriter("products.json", true)) {
+        	
+            file.write(obj.toJSONString());
+            file.flush();
 
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        System.out.print(obj);
     }
+//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+//
+//        List<Product> productList = new ArrayList<Product>();
+//        productList.add(product);
+//
+//        try (Writer writer = new FileWriter("products.json", true)) {
+//            writer.write(gson.toJson(productList));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 }
 
 
--- src/ch/gibm/gfjw/kasse/Main.java
+++ src/ch/gibm/gfjw/kasse/Main.java
@@ -1,10 +1,15 @@
 package ch.gibm.gfjw.kasse;
 
 import java.awt.BorderLayout;
+import java.util.ArrayList;
 
+import javax.rmi.CORBA.PortableRemoteObjectDelegate;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import ch.gibm.gfjw.data.ProductData;
+import ch.gibm.gfjw.dto.Product;
+import ch.gibm.gfjw.dto.ProductImpl;
 import ch.gibm.gfjw.gui.BackMoney;
 import ch.gibm.gfjw.gui.ManageProductPanel;
 import ch.gibm.gfjw.gui.View;
@@ -16,11 +21,11 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-//	View view = new View();
-	JFrame frame = new JFrame();
-	frame.setLayout(new BorderLayout());
-	JPanel mangProd = new BackMoney(200.0, new View());
-	frame.add(mangProd, BorderLayout.CENTER);
-	frame.setVisible(true);
+		ProductData data = new ProductData();
+		data.addProduct(new ProductImpl("Cola", 1, 2.00, 1.50));
+		data.addProduct(new ProductImpl("Fanta", 2, 4.00, 1.00));
+		
+//		System.out.println(data.getProductList().get(0).getName());
+//		System.out.println(data.getProductList().get(1).getName());
 	}
 }
