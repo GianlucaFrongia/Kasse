6686672bcf9413f82bd67e6868608a4877a55726
gianluca <gianluca.frongia@gibmit.ch>
2017-06-14 14:20:21 +0200

    ProductData abgeschlossen 4 Stunden

--- src/ch/gibm/gfjw/data/ProductData.java
+++ src/ch/gibm/gfjw/data/ProductData.java
@@ -12,6 +12,7 @@ import java.io.*;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -21,93 +22,52 @@ import org.json.simple.parser.ParseException;
  * Created by gianluca on 06.06.17
  */
 
-public class ProductData implements ProductDao{
+public class ProductData implements ProductDao {
 
+	ArrayList<Product> productList = new ArrayList<Product>();
+	private Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
-    public List<Product> getProductList() {
-    		
-    		ArrayList<Product> list = new ArrayList<>();
-    		JSONParser parser = new JSONParser();
+	public List<Product> getProductList() {
 
-        try {
+		try (Reader reader = new FileReader("products.json")) {
+			Type type = new TypeToken<List<ProductImpl>>() {
+			}.getType();
 
-            Object obj = parser.parse(new FileReader("products.json"));
+			productList = new Gson().fromJson(reader, type);
 
-            JSONObject jsonObject = (JSONObject) obj;
-            System.out.println(jsonObject);
-            
-            list.add(new ProductImpl((String) jsonObject.get("name"),
-            		(Integer) jsonObject.get("id"), 
-            		(Double) jsonObject.get("resale"), 
-            		(Double) jsonObject.get("price")));
+			return productList;
 
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-			// TODO Auto-generated catch block
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-        
-        return list;
 
-    }
-    	
-    	 	
-//        try (Reader reader = new FileReader("products.json")) {
-//            Type type = new TypeToken<List<ProductImpl>>(){}.getType();
-//
-//            List<Product> productList = new Gson().fromJson(reader, type);
-//
-//            return productList;
-//
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return null;
-//    }
+		return null;
+	}
 
+	public int deleteProduct(int id) {
+		getProductList();
+		Optional<Product> matchingProduct = productList.stream().filter(p -> p.getId() == id).findFirst();
+		Product p = matchingProduct.get();
+		productList.remove(p);
 
+		try (Writer writer = new FileWriter("products.json")) {
+			writer.write(gson.toJson(productList));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return id;
+	}
 
-    public int deleteProduct(int id) {
-        return id;
-    }
+	public void addProduct(Product product) {
+		getProductList();
 
-    public void addProduct(Product product) {
-    	
-    	JsonArray list = new JsonArray(obj);
-    	
-    	JSONObject obj = new JSONObject();
-        obj.put("id", product.getId());
-        obj.put("name", product.getName());
-        obj.put("resale", product.getResale());
-        obj.put("price", product.getPrice());
+		productList.add(product);
 
-        try (FileWriter file = new FileWriter("products.json", true)) {
-        	
-            file.write(obj.toJSONString());
-            file.flush();
+		try (Writer writer = new FileWriter("products.json")) {
+			writer.write(gson.toJson(productList));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.print(obj);
-    }
-//        Gson gson = new GsonBuilder().setPrettyPrinting().create();
-//
-//        List<Product> productList = new ArrayList<Product>();
-//        productList.add(product);
-//
-//        try (Writer writer = new FileWriter("products.json", true)) {
-//            writer.write(gson.toJson(productList));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
+	}
 }
-
-
