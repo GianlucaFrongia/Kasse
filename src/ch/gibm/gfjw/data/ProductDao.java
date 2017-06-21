bf82d0589867c41c6284b97b91f41d83e50cff90
gianluca <gianluca.frongia@gibmit.ch>
2017-06-21 12:59:01 +0200

    Added Javadoc, delete Product als void

--- src/ch/gibm/gfjw/data/ProductDao.java
+++ src/ch/gibm/gfjw/data/ProductDao.java
@@ -8,9 +8,9 @@ import java.util.List;
  * Created by gianluca on 07.06.17.
  */
 public interface ProductDao {
+	
     public List<Product> getProductList();
-
-    public int deleteProduct(int id);
+    public void deleteProduct(int id);
     public void addProduct(Product product);
 
 }
--- src/ch/gibm/gfjw/data/ProductData.java
+++ src/ch/gibm/gfjw/data/ProductData.java
@@ -19,14 +19,28 @@ import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 /**
- * Created by gianluca on 06.06.17
+ * ProductData Klasse
+ * Sucht, Löscht und schreibt Produkte in die JSON Datei
+ * 
+ * @author Gianluca Frongia und Joel Weissenberger
+ * @version 1.0
  */
 
 public class ProductData implements ProductDao {
-
+	/**
+	 * Produkte werden in productList geaddet
+	 */
 	ArrayList<Product> productList = new ArrayList<Product>();
+	
+	/**
+	 * Gson Objekt erstellen mit
+	 * setPrettyPrinting() für Lesbaren JSON
+	 * */
 	private Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
+	/**List die Json datei aus und Wandelt die JSON Produkt in Produkte Objekte
+	 * @return productList
+	 * */
 	public List<Product> getProductList() {
 
 		try (Reader reader = new FileReader("products.json")) {
@@ -44,7 +58,13 @@ public class ProductData implements ProductDao {
 		return null;
 	}
 
-	public int deleteProduct(int id) {
+	/**
+	 * Löscht ein Produkt.
+	 * Reihenfolge -> JSON lesen und in die ArrayList füllen -> Mit stream wird dann das Produkt mit der Passenden ID guscht und gelöscht
+	 * -> Anschliessend wird die JSON Datei geupdatet
+	 *@param int id 
+	 */
+	public void deleteProduct(int id) {
 		getProductList();
 		Optional<Product> matchingProduct = productList.stream().filter(p -> p.getId() == id).findFirst();
 		Product p = matchingProduct.get();
@@ -55,8 +75,13 @@ public class ProductData implements ProductDao {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return id;
 	}
+	
+	/***
+	 * getProductList Aufruf für die aktuellen Produkte
+	 * Anschliessend wir das Produkt in die ArrayList geaddet
+	 * @param Product
+	 * */
 
 	public void addProduct(Product product) {
 		getProductList();
